ccfacd12dc551577d115572db3d52736670d4d18
SSimonPegg <41246679+SSimonPegg@users.noreply.github.com>
2018-07-15 13:11:10 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,11 @@
 cd /v2raybin
-if [ "$VER" = "latest" ]; then
-  wget http://storage.googleapis.com/v2ray-docker/v2ray 
-  wget http://storage.googleapis.com/v2ray-docker/v2ctl
-  wget http://storage.googleapis.com/v2ray-docker/geoip.dat
-  wget http://storage.googleapis.com/v2ray-docker/geosite.dat
-else
-  wget -O v2ray.zip http://github.com/v2ray/v2ray-core/releases/download/v$VER/v2ray-linux-64.zip
-  unzip v2ray.zip 
-  cd /v2raybin/v2ray-v$VER-linux-64
-fi
+
+wget -O v2ray.zip http://github.com/v2ray/v2ray-core/releases/download/v$VER/v2ray-linux-64.zip
+
+unzip v2ray.zip 
+
+cd /v2raybin/v2ray-v$VER-linux-64
+
 
 chmod +x v2ray
 chmod +x v2ctl
